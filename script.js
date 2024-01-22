63052775fe26cfe1b88f82d7ed12fdf7331e3e3f
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2024-01-22 21:53:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+ function playSound(audiUrl){
+	 stopSound(); // Stop any currently playing sound
+
+      audio = new Audio(audioUrl);
+      audio.play();
+ }
+
+function stopSound() {
+      if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+      }
+    }
